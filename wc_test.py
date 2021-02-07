04074a27556568cf9d28ed915c55af5f12592e71
w.c <52445078@qq.com>
2021-02-07 16:59:38 +0800

    fix

---
+++ wc_test.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+# matplotlib.use('Agg')
+import datetime
+import pickle
+import itertools
+
+# %matplotlib inline
+from finrl.config import config
+from finrl.marketdata.yahoodownloader import YahooDownloader
+from finrl.preprocessing.preprocessors import FeatureEngineer
+from finrl.preprocessing.data import data_split
+from finrl.env.env_stocktrading import StockTradingEnv
+from finrl.model.models import DRLAgent
+
+from pprint import pprint
+
+import os
+if not os.path.exists("./" + config.DATA_SAVE_DIR):
+    os.makedirs("./" + config.DATA_SAVE_DIR)
+if not os.path.exists("./" + config.TRAINED_MODEL_DIR):
+    os.makedirs("./" + config.TRAINED_MODEL_DIR)
+if not os.path.exists("./" + config.TENSORBOARD_LOG_DIR):
+    os.makedirs("./" + config.TENSORBOARD_LOG_DIR)
+if not os.path.exists("./" + config.RESULTS_DIR):
+    os.makedirs("./" + config.RESULTS_DIR)
+
+fe = FeatureEngineer(
+                    use_technical_indicator=True,
+                    tech_indicator_list = config.TECHNICAL_INDICATORS_LIST,
+                    use_turbulence=False,
+                    user_defined_feature = False)
+
+df = pickle.load(open('./../dow_data.p', 'rb'))
+
+processed = fe.preprocess_data(df)
+
+list_ticker = processed["tic"].unique().tolist()
+list_date = list(pd.date_range(processed['date'].min(),processed['date'].max()).astype(str))
+combination = list(itertools.product(list_date,list_ticker))
+
+processed_full = pd.DataFrame(combination,columns=["date","tic"]).merge(processed,on=["date","tic"],how="left")
+processed_full = processed_full[processed_full['date'].isin(processed['date'])]
+processed_full = processed_full.sort_values(['date','tic'])
+
+processed_full = processed_full.fillna(0)
+
+train = data_split(processed_full, '2009-01-01','2019-01-01')
+trade = data_split(processed_full, '2019-01-01','2021-01-01')
+# print(len(train))
+
+stock_dimension = len(train.tic.unique())
+state_space = 1 + 2*stock_dimension + len(config.TECHNICAL_INDICATORS_LIST)*stock_dimension
+print(f"Stock Dimension: {stock_dimension}, State Space: {state_space}")
+
+env_kwargs = {
+    "hmax": 100, 
+    "initial_amount": 1000000, 
+    "buy_cost_pct": 0.001,
+    "sell_cost_pct": 0.001,
+    "state_space": state_space, 
+    "stock_dim": stock_dimension, 
+    "tech_indicator_list": config.TECHNICAL_INDICATORS_LIST, 
+    "action_space": stock_dimension, 
+    "reward_scaling": 1e-4
+    
+}
+
+e_train_gym = StockTradingEnv(df = train, **env_kwargs)
+
+env_train, _ = e_train_gym.get_sb_env()
+
+agent = DRLAgent(env = env_train)
+model_a2c = agent.get_model("a2c")
+
+trained_a2c = agent.train_model(model=model_a2c, 
+                             tb_log_name='a2c',
+                             total_timesteps=100000)
+
+### PPO ###
+
+# agent = DRLAgent(env = env_train)
+# PPO_PARAMS = {
+#     "n_steps": 2048,
+#     "ent_coef": 0.01,
+#     "learning_rate": 0.00025,
+#     "batch_size": 128,
+# }
+# model_ppo = agent.get_model("ppo",model_kwargs = PPO_PARAMS)
+
+# trained_ppo = agent.train_model(model=model_ppo, 
+#                              tb_log_name='ppo',
+#                              total_timesteps=100000)
+
+### PPO ###
+
+
+
+
+
+
+
+
